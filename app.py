5f5c490820f63dfddeddc8ab8f18875c1f0c5b3d
julialovescats <jmeiergr@iwu.edu>
2025-11-14 15:08:53 -0600

    added init_db functionality

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, g, redirect, url_for, render_template, flash
 
 # adapted from Flaskr
-# create our little application :)
 app = Flask(__name__)
 
 # Load default config and override config from an environment variable
@@ -18,6 +17,20 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -36,7 +49,7 @@ def close_db(error):
 
 # end adaption from Flaskr
 
-# will need to add water count manipulation later--separate function?
+# will need to add water count manipulation later--separate function? or just another execute?
 @app.route('/add_task', methods=['POST'])
 def add_task():
     db = get_db()
